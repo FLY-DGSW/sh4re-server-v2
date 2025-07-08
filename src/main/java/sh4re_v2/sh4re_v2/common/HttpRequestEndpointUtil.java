970c34f40e426c6576ef1cbb99aa2e83f0ed3556
Gorani <baconbacon1231@gmail.com>
2025-07-08 11:38:56 +0900

    feat: 존재하지 않는 엔드포인트에 대해선 404 반환

---
+++ src/main/java/sh4re_v2/sh4re_v2/common/HttpRequestEndpointUtil.java
@@ -0,0 +1,57 @@
+package sh4re_v2.sh4re_v2.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.HandlerMapping;
+import sh4re_v2.sh4re_v2.dto.BaseRes;
+import sh4re_v2.sh4re_v2.exception.ErrorResponse;
+import sh4re_v2.sh4re_v2.exception.error_code.CommonErrorCode;
+
+@Component
+@RequiredArgsConstructor
+public class HttpRequestEndpointUtil {
+  private final ObjectMapper objectMapper;
+  private final DispatcherServlet servlet;
+
+  private boolean isEndpointExist(HttpServletRequest request) {
+    List<HandlerMapping> handlerMappings = servlet.getHandlerMappings();
+    if(handlerMappings == null) return false;
+    handlerMappings = handlerMappings.subList(0, handlerMappings.size() - 1);
+    for (HandlerMapping handlerMapping : handlerMappings) {
+      try {
+        HandlerExecutionChain foundHandler = handlerMapping.getHandler(request);
+        System.out.println(foundHandler);
+        if (foundHandler != null) {
+          return true;
+        }
+      } catch (Exception e) {
+        return false;
+      }
+    }
+    return false;
+  }
+
+  public boolean isEndpointExistOrElseSetErrorResponse(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    if(!isEndpointExist(request)){
+      response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      ErrorResponse errorResponse = new ErrorResponse(
+          CommonErrorCode.ENDPOINT_NOT_FOUND.getCode(),
+          CommonErrorCode.ENDPOINT_NOT_FOUND.getMessage()
+      );
+      BaseRes<?> body = new BaseRes<>(false, "에러가 발생했습니다.", errorResponse);
+      objectMapper.writeValue(response.getOutputStream(), body);
+      return false;
+    }
+    return true;
+  }
+}
--- src/main/java/sh4re_v2/sh4re_v2/exception/error_code/CommonErrorCode.java
+++ src/main/java/sh4re_v2/sh4re_v2/exception/error_code/CommonErrorCode.java
@@ -9,7 +9,8 @@ import sh4re_v2.sh4re_v2.exception.exception.BusinessException;
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
   INVALID_ARGUMENT("INVALID_ARGUMENT", "유효하지 않은 인자입니다.", HttpStatus.BAD_REQUEST),
-  INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "서버에 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),;
+  INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", "서버에 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+  ENDPOINT_NOT_FOUND("ENDPOINT_NOT_FOUND", "존재하지 않는 엔드포인트입니다.", HttpStatus.NOT_FOUND),;
 
   private final String code;
   private final String message;
