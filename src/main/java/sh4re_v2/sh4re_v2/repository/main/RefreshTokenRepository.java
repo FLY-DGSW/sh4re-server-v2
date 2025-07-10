acc1747b84e8fd2bcbbb5e111864a6f1ad472b5b
Gorani <baconbacon1231@gmail.com>
2025-07-10 19:31:08 +0900

    feat: 레포지토리 추가

--- src/main/java/sh4re_v2/sh4re_v2/repository/main/RefreshTokenRepository.java
+++ src/main/java/sh4re_v2/sh4re_v2/repository/main/RefreshTokenRepository.java
@@ -1,8 +1,8 @@
-package sh4re_v2.sh4re_v2.repository;
+package sh4re_v2.sh4re_v2.repository.main;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import sh4re_v2.sh4re_v2.domain.UserRefreshToken;
+import sh4re_v2.sh4re_v2.domain.main.UserRefreshToken;
 
 public interface RefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
   Optional<UserRefreshToken> findByUsername(String username);
---
+++ src/main/java/sh4re_v2/sh4re_v2/repository/tenant/SubjectRepository.java
@@ -0,0 +1,10 @@
+package sh4re_v2.sh4re_v2.repository.tenant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sh4re_v2.sh4re_v2.domain.tenant.Subject;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+
+}
