bafc9e41a9daa96f7514d865fce9a78f90cdbc89
Gorani <baconbacon1231@gmail.com>
2025-07-12 22:19:59 +0900

    feat: School 테넌트 폴더 구조 변경

--- src/main/java/sh4re_v2/sh4re_v2/tenant/SchoolRoutingDataSource.java
+++ src/main/java/sh4re_v2/sh4re_v2/tenant/SchoolRoutingDataSource.java
@@ -1,11 +1,12 @@
 package sh4re_v2.sh4re_v2.tenant;
 
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import sh4re_v2.sh4re_v2.context.TenantContext;
 
 public class SchoolRoutingDataSource extends AbstractRoutingDataSource {
   @Override
   protected Object determineCurrentLookupKey() {
-    String tenantId = SchoolContextHolder.getTenantId();
+    String tenantId = TenantContext.getTenantId();
 
     if (tenantId == null) {
       throw new IllegalStateException("[🛑 오류] 현재 요청에 schoolId가 설정되지 않았습니다.");
